14b3af5ed5dda73d2faf1b53b838cc607e67b064
hrd <843736012@qq.com>
2024-07-26 17:41:48 +0800

    指令完善

---
+++ src/dataProcessing/objConvertString.ts
@@ -0,0 +1,90 @@
+import { DirectiveTree } from "../types";
+
+export const config: DirectiveTree = {
+  name: "dataProcessing.objConvertString",
+  displayName: "对象转字符串",
+  icon: "icon-web-create",
+  isControl: false,
+  isControlEnd: false,
+  comment: "输出日志 ${content}",
+  inputs: {
+    obj: {
+      name: "obj",
+      value: "",
+      type: "variable",
+      addConfig: {
+        label: "转换的对象",
+        type: "variable",
+        placeholder: "请输入转换的对象",
+        defaultValue: "",
+        tip: "转换的对象",
+      },
+    },
+  },
+  outputs: {
+    content: {
+      name: "content",
+      type: "string",
+      display: "转换结果",
+      addConfig: {
+        label: "转换结果",
+        type: "variable",
+        tip: "转换结果",
+      },
+    },
+  },
+};
+
+const conver = (content: any) => {
+  const cacheMap = new Map();
+  const convertMapToObject = (map: Map<any, any>) => {
+    const obj: Record<any, any> = {};
+    for (const [key, value] of map.entries()) {
+      obj[key] = ((value) => {
+        const cacheKey = value;
+        if (cacheMap.has(cacheKey)) {
+          return cacheMap.get(cacheKey);
+        }
+
+        if (value instanceof Map) {
+          return convertMapToObject(value);
+        } else if (Array.isArray(value)) {
+          value = value.map((item) => {
+            if (item instanceof Map) {
+              return convertMapToObject(item);
+            }
+            return item;
+          });
+          cacheMap.set(cacheKey, value);
+          return value;
+        }
+
+        cacheMap.set(cacheKey, value);
+        return value;
+      })(value);
+    }
+    return obj;
+  };
+
+  if (content instanceof Map) {
+    content = convertMapToObject(content);
+  } else if (Array.isArray(content)) {
+    const arr = [];
+    for (let item of content) {
+      if (item instanceof Map) {
+        arr.push(convertMapToObject(item));
+      } else {
+        arr.push(item);
+      }
+    }
+    content = arr;
+  }
+
+  cacheMap.clear();
+  return content;
+};
+
+exports.impl = async function ({ obj }: { obj: any }) {
+  const content = JSON.stringify(conver(obj), null, 2);
+  return { content };
+};
