727ceb55ded6bef296a2e41f0a20acf7118a618d
hada <754781341@qq.com>
2024-07-30 16:01:25 +0800

    调用子流程参数传递

--- src/flowApp/addSubFlowReturnVal.ts
+++ src/flowApp/addSubFlowReturnVal.ts
@@ -1,39 +1,40 @@
-import { DirectiveTree } from "../types";
+import { DirectiveTree } from '../types';
 
 export const config: DirectiveTree = {
-	name: "flowApp.addSubFlowReturnVal",
-	displayName: "子流程添加返回值",
-	icon: "icon-web-create",
-	isControl: false,
-	isControlEnd: false,
-	comment: "添加返回值${val}到返回值列表中",
-	inputs: {
-		val: {
-			name: "val",
-			value: "",
-			type: "variable",
-			addConfig: {
-				label: "添加要返回的变量",
-				type: "variable",
-				required: true,
-				tip: "添加要返回的内容",
-			},
-		},
-	},
-	outputs: {},
-	async toCode(directive, block) {
-		const { val } = directive.inputs;
-		return `await robotUtil.system.flowApp.addSubFlowReturnVal({val: ${val.value} },returnVal,${block})`;
-	},
+    name: 'flowApp.addSubFlowReturnVal',
+    displayName: '子流程添加返回值',
+    icon: 'icon-web-create',
+    isControl: false,
+    isControlEnd: false,
+    comment: '添加返回值${val}到返回值列表中',
+    inputs: {
+        val: {
+            name: 'val',
+            value: '',
+            type: 'array',
+            addConfig: {
+                label: '添加要返回的变量列表',
+                type: 'variable',
+                multiple: true,
+                required: true,
+                tip: '添加要返回的内容'
+            }
+        }
+    },
+    outputs: {},
+    async toCode(directive, block) {
+        const { val } = directive.inputs;
+        return `await robotUtil.system.flowApp.addSubFlowReturnVal({val: [${val.value}] },returnVal,${block})`;
+    }
 };
 
 export const impl = async function (
-	{
-		val,
-	}: {
-		val: any;
-	},
-	returnVal: any[]
+    {
+        val
+    }: {
+        val: any;
+    },
+    returnVal: any[]
 ) {
-	returnVal.push(val);
+    returnVal.push(...val);
 };
